54046d92bb036342514bf5c196a04897ef24b439
santhosh320 <55526035+santhosh320@users.noreply.github.com>
2020-08-20 19:49:35 +0530

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,17 @@
+#1
+l=[1,2,3,4,5,6]
+l1=l[::-2]
+l2=l[-2::-2]
+l2[0]=2
+print(l1)
+print(l2)
+
+
+#2
+r='abcdef'
+s='aaaaaa'
+t='a'
+t=t.replace('a','b')
+print (r.replace('a',t))
+print (s.replace('a',t))
+
